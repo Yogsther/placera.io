a1a2db81a0966fd1fa2a383ab7cb282b8c49f937
Olle Kaiser <Olle@Livingfor.it>
2017-10-27 23:58:07 +0200

    Update

--- _server/index.js
+++ _server/index.js
@@ -12,14 +12,28 @@ app.use(express.static("public"))
 
 var io = socket(server);
 
+// Pixels cache
+var pixels = [];
 
 io.on("connection", function(socket){
   console.log("User connected");
+  io.sockets.connected[socket.id].emit("cache", pixels);
+
+
+
 
 socket.on('disconnect', function(){
   console.log("User disconnected");
-
-
   });
+
+
+  // Placera.io handler
+
+socket.on("newpixel", function(newPixel){
+    console.log(pixels);
+    pixels.push(newPixel);
+    socket.emit("update", newPixel);
+});
+
 
 });
--- placera.js
+++ placera.js
@@ -6,6 +6,8 @@ var socket = io.connect("http://localhost:8080");
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+var pixels = [];
+
 var mouseX;
 var mouseY;
 
@@ -21,19 +23,17 @@ function getMousePos(canvas, evt) {
 canvas.addEventListener('mousemove', function(evt) {
 
       var mousePos = getMousePos(canvas, evt);
-
-      mouseX = mousePos.x;
-      mouseY = mousePos.y;
+      mouseX = mousePos.x -1;
+      mouseY = mousePos.y -1;
       // mousePos.x + mousePos.y
 }, false);
-
 
 // Run
 setInterval(update, 16);
 
+
 // Hide cursor?
 //document.body.style.cursor = 'none';
-
 
 
 function update(){
@@ -41,14 +41,20 @@ function update(){
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  mouseX = mouseX / 10;
-  mouseY = mouseY / 10;
-
-  mouseX = Math.floor(mouseX);
-  mouseY = Math.floor(mouseY);
-
+  mouseX = Math.round(mouseX / 10);
   mouseX = mouseX * 10;
+
+  mouseY = Math.round(mouseY / 10);
   mouseY = mouseY * 10;
+
+
+
+  var i = 0;
+  while(i < pixels.length){
+    ctx.fillStyle = pixels[i].color;
+    ctx.fillRect(pixels[i].x, pixels[i].y, 10, 10);
+    i++;
+  }
 
   ctx.fillStyle = "rgba(0,0,0,.5)";
   ctx.fillRect(mouseX, mouseY, 10, 10);
@@ -56,3 +62,31 @@ function update(){
   console.log("update");
 
 }
+
+// Place pixels user
+canvas.addEventListener("click", function(){
+
+  mouseX = Math.round(mouseX / 10);
+  mouseX = mouseX * 10;
+
+  mouseY = Math.round(mouseY / 10);
+  mouseY = mouseY * 10;
+
+  var  newPixel = {
+    x: mouseX,
+    y: mouseY,
+    color: "black"
+  };
+
+  socket.emit("newpixel", newPixel);
+
+});
+
+// Update on new pixel
+socket.on("update", function(pixel){
+  pixels.push(pixel);
+});
+
+socket.on("cache", function(allPixels){
+  pixels = allPixels;
+});
